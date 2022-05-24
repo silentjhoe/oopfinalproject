6e1c05a3c294c6af2c1aac34377507e1b50c35ba
Jhoe Alfe Mario Bagtong-PH <83755315+silentjhoe@users.noreply.github.com>
2022-05-24 16:10:29 +0800

    ThisUpdate

--- FINAL_PROJECT/Form1.vb
+++ FINAL_PROJECT/Form1.vb
@@ -14,5 +14,6 @@
 
     Private Sub LinkLabel1_LinkClicked(sender As Object, e As LinkLabelLinkClickedEventArgs) Handles LinkLabel1.LinkClicked
         Form2.Show()
+        Me.Hide()
     End Sub
 End Class
